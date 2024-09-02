2ff35abe50bfcb75055f4e83957c79e7bb7c46eb
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-09-02 13:05:08 +0300

    add main

---
+++ export.py
@@ -0,0 +1,17 @@
+# pip3 install tensorflow==2.15.0 tf2onnx onnx
+# python main.py Nakdimon.h5
+# Note: model requires special load function so I converted it using nakdimon own functions to load it.
+
+import tensorflow as tf
+import tf2onnx
+import onnx
+from tensorflow import keras
+from keras.models import load_model
+import sys
+
+path = sys.argv[1]
+model = load_model(path)
+
+print('model loaded')
+onnx_model, _ = tf2onnx.convert.from_keras(model, ustom_objects={'loss': None})
+onnx.save(onnx_model, 'nakdimon.onnx')
