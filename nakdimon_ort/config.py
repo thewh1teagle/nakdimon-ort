2f6ebfa60f6118040aaf7f39358847815e71665c
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-09-05 19:43:10 +0300

    refactor

--- nakdimon_ort/config.py
+++ nakdimon_ort/config.py
@@ -1,23 +1,39 @@
 import json
 from pathlib import Path
 
-DEFAULT_CONFIG_PATH = Path(__file__).parent / "config.json"
+class Config:
+    def __init__(self, model_path, config_path: str) -> None:
+        config = self.load(model_path, config_path)
+        self.rafe: str = config["rafe"]
+        self.niqqud: list = config["niqqud"]
+        self.dagesh: list = config["dagesh"]
+        self.sin: list = config["sin"]
+        self.hebrew_letters: list = config["hebrew"]
+        self.valid: list = config["valid"] + self.hebrew_letters
+        self.special: list = config["special"]
+        self.normalize_map: dict = config["normalize_map"]
+        self.normalize_deafult_value: str = self.normalize_map["default"]
+        self.can_dagesh: list = config["can_dagesh"]
+        self.can_sin: list = config["can_sin"]
+        self.can_niqqud: list = config["can_niqqud"]
+        self.all_tokens: list = [""] + self.special + self.valid
+        self.remove_niqqud_range: list = config["remove_niqqud_range"]
+        self.max_len: int = config["max_len"]
+        
+    def load(self, model_path, config_path):
+        assert Path(model_path).exists(), (
+            f"Model file not found: {model_path}\n"
+            "Please download the Nakdimon model before executing.\n"
+            "You can download it using the following command:\n"
+            "wget https://github.com/thewh1teagle/nakdimon-ort/releases/download/v0.1.0/nakdimon.onnx"
+        )
 
+        assert Path(config_path).exists(), (
+            f"Configuration file not found: {config_path}\n"
+            "Please download the Nakdimon configuration file before executing.\n"
+            "You can download it using the following command:\n"
+            "wget https://github.com/thewh1teagle/nakdimon-ort/raw/main/nakdimon_ort/config.json"
+        )
 
-def load_config(model_path, config_path=Path(__file__).parent / "config.json"):
-    assert Path(model_path).exists(), (
-        f"Model file not found: {model_path}\n"
-        "Please download the Nakdimon model before executing.\n"
-        "You can download it using the following command:\n"
-        "wget https://github.com/thewh1teagle/nakdimon-ort/releases/download/v0.1.0/nakdimon.onnx"
-    )
-
-    assert Path(config_path).exists(), (
-        f"Configuration file not found: {config_path}\n"
-        "Please download the Nakdimon configuration file before executing.\n"
-        "You can download it using the following command:\n"
-        "wget https://github.com/thewh1teagle/nakdimon-ort/raw/main/nakdimon_ort/config.json"
-    )
-
-    with open(config_path, "r", encoding="utf-8") as f:
-        return json.load(f)
+        with open(config_path, "r", encoding="utf-8") as f:
+            return json.load(f)
--- nakdimon_ort/model.py
+++ nakdimon_ort/model.py
@@ -1,56 +1,42 @@
 import numpy as np
 import onnxruntime as ort
-from .config import load_config, DEFAULT_CONFIG_PATH
-
+from .config import Config
+from pathlib import Path
 
 class Nakdimon:
-    def __init__(self, model_path, config_path=DEFAULT_CONFIG_PATH):
-        self.config = load_config(model_path, config_path)
-        self.rafe = self.config["rafe"]
-        self.niqqud = self.config["niqqud"]
-        self.dagesh = self.config["dagesh"]
-        self.sin = self.config["sin"]
-        self.hebrew_letters = self.config["hebrew"]
-        self.valid = self.config["valid"] + self.hebrew_letters
-        self.special = self.config["special"]
-        self.normalize_map = self.config["normalize_map"]
-        self.normalize_deafult_value = self.normalize_map["default"]
-        self.can_dagesh = self.config["can_dagesh"]
-        self.can_sin = self.config["can_sin"]
-        self.can_niqqud = self.config["can_niqqud"]
-        self.all_tokens = [""] + self.special + self.valid
-        self.remove_niqqud_range = self.config["remove_niqqud_range"]
-        self.max_len = self.config["max_len"]
+    def __init__(self, model_path, config_path = Path(__file__).parent / "config.json"):
+        self.config = Config(model_path, config_path)
         self.session = ort.InferenceSession(model_path)
 
     def normalize(self, c):
         return (
-            self.normalize_map.get(c, self.normalize_deafult_value)
-            if c not in self.valid
+            self.config.normalize_map.get(c, self.config.normalize_deafult_value)
+            if c not in self.config.valid
             else c
         )
 
     def split_to_rows(self, text):
         word_ids_rows = [
-            [self.all_tokens.index(c) for c in word] for word in text.split(" ")
+            [self.config.all_tokens.index(c) for c in word] for word in text.split(" ")
         ]  # Convert text to token IDs
         rows, cur_row = [], []
 
         for word_ids in word_ids_rows:
             # Check if adding the word exceeds the max length
-            if len(cur_row) + len(word_ids) + 1 > self.max_len:
-                padding = [0] * (self.max_len - len(cur_row))
+            if len(cur_row) + len(word_ids) + 1 > self.config.max_len:
+                padding = [0] * (self.config.max_len - len(cur_row))
                 rows.append(cur_row + padding)  # Pad and save the current row
                 cur_row = []
             cur_row.extend(
-                word_ids + [self.all_tokens.index(" ")]
+                word_ids + [self.config.all_tokens.index(" ")]
             )  # Add the word and space to the current row
 
         # Final padding and appending of the last row
-        rows.append(cur_row + [0] * (self.max_len - len(cur_row)))
+        rows.append(cur_row + [0] * (self.config.max_len - len(cur_row)))
         return rows
 
     def from_categorical(self, input_tensor, arr: list):
+        # Filter zeros from input_tensor
         return np.argmax(arr[input_tensor > 0], axis=-1).flatten()
 
     def prediction_to_text(self, input_tensor, prediction, undotted_text):
@@ -61,13 +47,13 @@ class Nakdimon:
         output = []
         for i, c in enumerate(undotted_text):
             fresh = {"char": c, "niqqud": "", "dagesh": "", "sin": ""}
-            if c in self.hebrew_letters:
-                if c in self.can_niqqud:
-                    fresh["niqqud"] = self.niqqud[niqqud_result[i]]
-                if c in self.can_dagesh:
-                    fresh["dagesh"] = self.dagesh[dagesh_result[i]]
-                if c in self.can_sin:
-                    fresh["sin"] = self.sin[sin_result[i]]
+            if c in self.config.hebrew_letters:
+                if c in self.config.can_niqqud:
+                    fresh["niqqud"] = self.config.niqqud[niqqud_result[i]]
+                if c in self.config.can_dagesh:
+                    fresh["dagesh"] = self.config.dagesh[dagesh_result[i]]
+                if c in self.config.can_sin:
+                    fresh["sin"] = self.config.sin[sin_result[i]]
             output.append(fresh)
         return output
 
@@ -76,7 +62,7 @@ class Nakdimon:
             [
                 c
                 for c in text
-                if not (self.remove_niqqud_range[0] <= c <= self.remove_niqqud_range[1])
+                if not (self.config.remove_niqqud_range[0] <= c <= self.config.remove_niqqud_range[1])
             ]
         )
 
